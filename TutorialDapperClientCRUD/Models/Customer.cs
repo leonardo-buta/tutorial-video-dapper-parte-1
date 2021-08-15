d137ec5167cc73a2c376e03dac33d50f222a1f11
Leonardo Buta <leonardo.lbuta@gmail.com>
2021-08-15 18:06:49 -0300

    Initial

---
+++ TutorialDapperClientCRUD/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorialDapperClientCRUD.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ TutorialDapperClientCRUD/Repository/CustomerRepository.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using TutorialDapperClientCRUD.Models;
+
+namespace TutorialDapperClientCRUD.Repository
+{
+    public class CustomerRepository
+    {
+        private readonly IConfiguration configuration;
+
+        public CustomerRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            var sql = "SELECT * FROM Customers WHERE CustomerId = @Id";
+
+            using var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            var result = await connection.QuerySingleOrDefaultAsync<Customer>(sql, new { Id = id });
+            return result;
+        }
+
+        public async Task<IReadOnlyList<Customer>> GetAllAsync()
+        {
+            var sql = "SELECT * FROM Customers";
+
+            using var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            var result = await connection.QueryAsync<Customer>(sql);
+            return result.ToList();
+        }
+
+        public async Task<Customer> AddAsync(Customer entity)
+        {
+            entity.CreationDate = DateTime.Now;
+            var sql = "INSERT INTO Customers (Name, Phone, CreationDate) VALUES (@name, @phone, @creationDate);" +
+                      "SELECT CAST(SCOPE_IDENTITY() as int)";
+
+            using var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            entity.CustomerId = await connection.QuerySingleAsync<int>(sql, entity);
+            return entity;
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var sql = "DELETE FROM Customers WHERE CustomerId = @id";
+            using var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            var result = await connection.ExecuteAsync(sql, new { id });
+            return result;
+        }
+
+        public async Task<int> UpdateAsync(Customer entity)
+        {
+            var sql = "UPDATE Customers SET Name = @name, Phone = @phone WHERE CustomerId = @CustomerId";
+            using var connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            connection.Open();
+
+            var result = await connection.ExecuteAsync(sql, entity);
+            return result;
+        }
+    }
+}
